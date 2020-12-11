d3428c33d64a070d7d9b894c768e7f0360f5a7a4
Matthew O. Smith <matt@m0smith.com>
2020-12-11 07:55:34 -0700

    shell and arc-mode

--- wsl.el
+++ wsl.el
@@ -51,6 +51,24 @@ windows box and it has WSL installed."
   :type '(repeat string)
   :group 'wsl)
 
+(defcustom wsl-on-hook '(wsl-update-jka-compra-on
+			 wsl-grep-on
+			 wsl-shell-on
+			 wsl-archive-on
+			 wsl-subr-on)
+  "List of functions to call when WSL mode is enabled"
+  :group 'wsl
+  :type 'hook)
+
+(defcustom wsl-off-hook '(wsl-update-jka-compra-off
+			  wsl-grep-off
+			  wsl-shell-off
+			  wsl-archive-off
+			  wsl-subr-off)
+  "List of functions to call when WSL mode is disabled"
+  :group 'wsl
+  :type 'hook)
+
 (defvar wsl-jka-compr-compression-info-list jka-compr-compression-info-list
   "Hold onto the original value of `jka-compr-compression-info-list'")
 
@@ -60,6 +78,10 @@ windows box and it has WSL installed."
   (let ((wsl-exe (expand-file-name wsl-command)))
     (when (and wsl-exe (file-exists-p wsl-exe))
       wsl-exe)))
+
+;;;
+;; jka-compra
+;;;
 
 (defun wsl-update-jka-compra-el (program args)
   (if (member program wsl-jka-compra-compress-programs)
@@ -76,7 +98,7 @@ windows box and it has WSL installed."
 		uncompress-msg ucp uca
 		append-flag strip-extension-flag file-magic-chars)))))
 
-(defun wsl-update-jka-compra-on ()
+(defun wsl-update-jka-compra-on (wsl-exe)
   (setq jka-compr-compression-info-list (mapcar 'wsl-update-jka-compra jka-compr-compression-info-list))
   (jka-compr-update))
 
@@ -84,31 +106,126 @@ windows box and it has WSL installed."
   (setq jka-compr-compression-info-list wsl-jka-compr-compression-info-list)
   (jka-compr-update))
 
+
+;;;;
+;; subr
+;;;;
+
+(defun wsl-shell-quote-argument (arg)
+  (message "wsl-shell-quote-argument : %s" arg)
+  (when-let ((pre (string-match "^\\([a-zA-Z]\\):[/\\]\\(.*\\)$" arg)))
+    (format "/mnt/%s/%s" (downcase (match-string 1 arg)) (match-string 2 arg))
+  ))
+
+(defun wsl-subr-on (wsl-exe)
+  (advice-add 'shell-quote-argument :before-until #'wsl-shell-quote-argument))
+
+(defun wsl-subr-off ()
+  (advice-remove 'shell-quote-argument #'wsl-shell-quote-argument))
+
+;;;
+;; archive
+;;;
+
+(defvar wsl-archive-zip-extract archive-zip-extract)
+
+(defun wsl-archive-extract-by-stdout (old-fn archive &rest args)
+  (apply old-fn (wsl-shell-quote-argument archive) args))
+
+(defun wsl-archive-on (wsl-exe)
+  (advice-add 'archive-extract-by-stdout :around #'wsl-archive-extract-by-stdout))
+  (setq archive-zip-extract (cons wsl-exe archive-zip-extract)))
+
+(defun wsl-archive-off ()
+  (advice-remove 'archive-extract-by-stdout #'wsl-archive-extract-by-stdout))
+  (setq archive-zip-extract wsl-archive-zip-extract))
+
+  
+
+;;
+;; SHELL
+;;
+
+(defvar wsl-shell-file-name shell-file-name)
+(defvar wsl-shell-command-switch shell-command-switch)
+
+(defun wsl-shell-on (wsl-exe)
+  (setq shell-command-switch "-c"
+	explicit-wsl.exe-args (list wsl-exe)
+	explicit-cmdproxy.exe-args (list wsl-exe)
+	;;shell-file-name wsl-exe
+	))
+
+(defun wsl-shell-off ()
+  (setq shell-command-switch wsl-shell-command-switch
+	shell-file-name wsl-shell-file-name
+	))
+
+;;
+;; TRAMP
+;;
+
+(defvar wsl-tramp-methods tramp-methods)
+
+(defun wsl-tramp-on (wsl-exe)
+  (push
+   (cons
+    "wsl"
+    (list (list 'tramp-login-program wsl-exe)
+	  '(tramp-login-args (("-e") ("/bin/bash" "-il")))
+	  (list 'tramp-remote-shell "/bin/sh -i")
+	  '(tramp-remote-shell-login ("-il"))
+	  '(tramp-remote-shell-args ("-i" "-c"))
+      ))
+   tramp-methods)
+  )
+
+(defun wsl-tramp-off ()
+  
+  (setq tramp-methods wsl-tramp-methods))
+
+;;
+;; grep
+;;
+(defvar wsl-null-device null-device)
+
+(defun wsl-grep-on (wsl-exe)
+  (setq null-device "/dev/null")
+  (grep-apply-setting 'grep-command (format "%s grep -n " wsl-exe))
+  (grep-apply-setting 'grep-template (format "%s grep <X> <C> -n <R> <F>" wsl-exe))
+  (grep-apply-setting 'grep-find-command
+		      (cons (format "%s find . -type f -exec grep  -n  \"{}\" /dev/null \";\"" wsl-exe) 60))
+  (grep-apply-setting 'grep-find-template
+		      (format "%s find <D> <X> -type f <F> -exec grep <C> -n <R> \"{}\" /dev/null \";\"" wsl-exe))
+  (grep-compute-defaults)
+  )
+
+(defun wsl-grep-off ()
+  (setq null-device wsl-null-device
+	grep-host-defaults-alist nil
+	grep-command nil
+	grep-template nil
+	grep-find-command nil
+	grep-find-template nil)
+  (grep-compute-defaults) )
+
+;;
+;; minor mode
+;;
+
 (defun wsl-mode-on ()
   "What to do when function `wsl-mode' is enabled."
   (let ((wsl-exe (wsl-p)))
     (when wsl-exe
-      (wsl-update-jka-compra-on)
-      (grep-apply-setting 'grep-command (format "%s grep  -n " wsl-exe))
-      (grep-apply-setting 'grep-template (format "%s grep <X> <C> -n <R> <F>" wsl-exe))
-      (grep-apply-setting 'grep-find-command
-			  (cons (format "%s find . -type f -exec grep  -n  \"{}\" /dev/null \";\"" wsl-exe) 60))
-      (grep-apply-setting 'grep-find-template
-			  (format "%s find <D> <X> -type f <F> -exec grep <C> -n <R> \"{}\" /dev/null \";\"" wsl-exe))
-      
+      (run-hook-with-args 'wsl-on-hook wsl-exe)
       (setq cider-clojure-cli-command wsl-exe))))
 
 ;; "c:/windows/sysnative/wsl.exe" clojure -Sdeps '{:deps {nrepl {:mvn/version "0.8.2"} refactor-nrepl {:mvn/version "2.5.0"} cider/cider-nrepl {:mvn/version "0.25.4"}}}' -m nrepl.cmdline --middleware '["refactor-nrepl.middleware/wrap-refactor", "cider.nrepl/cider-middleware"]'
 
 (defun wsl-mode-off ()
   "What to do when function `wsl-mode' is disabled."
-  (wsl-update-jka-compra-off)
-  (setq grep-host-defaults-alist nil
-	grep-command nil
-	grep-template nil
-	grep-find-command nil
-	grep-find-template nil)
-  (grep-compute-defaults))
+  (run-hooks 'wsl-off-hook))
+
 
 ;;;###autoload
 (define-minor-mode wsl-mode
